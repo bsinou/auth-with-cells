e7d6dbd98cbb9a4f96389abea9c57c038f519d71
Bruno Sinou <bruno.sinou@posteo.de>
2019-11-21 13:34:54 +0100

    Only expose static files at website root to test packr deployment

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/gobuffalo/packr"
-
-	"github.com/bsinou/auth-with-cells/backend"
 )
 
 func main() {
 	router := gin.Default()
 
-	// Serve ReactJs compiled files with Packr under /ui prefix
+	// Test serving static file when deployed on the server
 	box := packr.NewBox("./frontend/build")
-	router.StaticFS("/ui", box)
-	// and redirect the root of the website
-	router.GET("/", func(c *gin.Context) {
-		c.Redirect(http.StatusMovedPermanently, "/ui")
-	})
+	router.StaticFS("/", box)
 
-	// Dummy auth entry point
-	authG := router.Group("/auth")
-	{
-		authG.POST("/login", backend.PerformDummyLogin)
-	}
+	// // Serve ReactJs compiled files with Packr under /ui prefix
+	// box := packr.NewBox("./frontend/build")
+	// router.StaticFS("/ui", box)
+	// // and redirect the root of the website
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.Redirect(http.StatusMovedPermanently, "/ui")
+	// })
 
-	// Authenticated routes
-	api := router.Group("/api")
-	{
-		api.GET("/", backend.Hello)
-	}
+	// // Dummy auth entry point
+	// authG := router.Group("/auth")
+	// {
+	// 	authG.POST("/login", backend.PerformDummyLogin)
+	// }
+
+	// // Authenticated routes
+	// api := router.Group("/api")
+	// {
+	// 	api.GET("/", backend.Hello)
+	// }
 
 	// Starts the server in an infinite loop => not production ready.
 	router.Run(":8888")
